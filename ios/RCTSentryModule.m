8013beda14578bb81d86cf12259f50dee14dcd6b
Matthew Mitchener <matthew.mitchener@abbott.com>
2024-01-18 16:07:18 -0800

    Update debugging code

--- ios/RCTSentryModule.m
+++ ios/RCTSentryModule.m
@@ -2,25 +2,22 @@
 #import <Sentry/SentrySDK.h>
 #import <React/RCTLog.h>
 #import <React/RCTBridgeModule.h>
+#import "AppDelegate.h"
 
 @implementation RCTSentryModule
 
 RCT_EXPORT_MODULE()
-- (void)aMethodThatMightFail:(NSError **)error {
-      NSArray *array = @[];
-      id object = array[1]; // This will trigger an NSRangeException
-}
 
 RCT_EXPORT_METHOD(captureMessage:(NSString *)message)
 {
     RCTLogInfo(@"%@", message);
-    NSError *error = nil;
-    RCTLogInfo(@"%@", @"Will it fail?");
-    [self aMethodThatMightFail:&error];
-    RCTLogInfo(@"%@", @"Bad!");
-    if (error) {
-        [SentrySDK captureError:error];
-    }
+//    @try {
+        NSArray *array = @[];
+        id object = array[1]; // This will trigger an NSRangeException
+//    } @catch (NSException *exception) {
+//        RCTLogInfo(@"%@", exception);
+//        [SentrySDK captureException:exception];
+//    }
 }
 
 @end
